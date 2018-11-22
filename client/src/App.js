d61b9d5972d06dd2f04d83ba8749bbd4b9c784d8
Alexander Thompson <legoboyjr@gmail.com>
2018-11-21 20:00:21 -0500

    extracted weather info on the main page

--- client/src/App.js
+++ client/src/App.js
@@ -3,30 +3,35 @@ import axios from 'axios';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       weather: {},
       error: null
     }
+    this.fetchWeather = this.fetchWeather.bind(this);
   }
-  componentDidMount(){
-    axios.get('/forecast/coords/0,0')
-    .then(response => {
-      this.setState({
-        weather: response.data.weather
+  componentDidMount() {
+    this.fetchWeather(0,0);
+  }
+  fetchGeoLocation() {}
+  fetchWeather(lat, lon) {
+    axios.get(`/forecast/coords/${lat},${lon}`)
+      .then(response => {
+        this.setState({
+          weather: response.data.weather
+        })
       })
-    })
-    .catch(err => {
-      this.setState({
-        error: err
-      })
-    });
+      .catch(err => {
+        this.setState({
+          error: err
+        })
+      });
   }
   render() {
     return (
-      <div className="App">
-      it works
+      <div>
+        <pre>{JSON.stringify(this.state.weather, null, 2)}</pre>
       </div>
     );
   }
