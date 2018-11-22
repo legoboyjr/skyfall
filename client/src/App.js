56f7929da8a8958c372a54a9bf2366ea65a68713
Alexander Thompson <legoboyjr@gmail.com>
2018-11-21 19:48:05 -0500

    added axios to the client project set up component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      weather: {},
+      error: null
+    }
+  }
+  componentDidMount(){
+    axios.get('/forecast/coords/0,0')
+    .then(response => {
+      this.setState({
+        weather: response.data.weather
+      })
+    })
+    .catch(err => {
+      this.setState({
+        error: err
+      })
+    });
+  }
   render() {
     return (
       <div className="App">
