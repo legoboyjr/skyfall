85596c28cb3b7c3ec4c4db4eb36796caf45ea7b5
Alexander Thompson <legoboyjr@gmail.com>
2018-12-03 19:18:03 -0500

    dayly weather

--- client/src/DailyWeather.js
+++ client/src/DailyWeather.js
@@ -1,15 +1,18 @@
 import React from 'react'
 
-
+const renderDay = day =>
+    <div key={day.temp}>
+        <h3 className='m-0'> Monday: {day.apparentTemperatureHigh} &deg;F</h3>
+        <hr />
+    </div>
+    
 function DailyWeather(props) {
     const { data } = props.data;
-    const firstDay = data[0];
-    const { apparentTemperatureMax } = firstDay;
+    const items = data.map(renderDay)
     return (
-        <div className='d-flex justify-content-center pt-5 bg-primary border-top border-dark flex-grow-1'>
-            <h3 className='m-0'> The first day forcast: {apparentTemperatureMax} &deg;F</h3>
+        <div className='d-flex align-items-center pt-5 bg-primary border-top border-dark flex-grow-1 flex-column'style={{ overflowY: 'scroll' }}>
+            {items}
         </div>
-        
     )
 }
 
